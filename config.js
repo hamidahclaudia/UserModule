c0918b76cb9b0941a19cd84dcd406bbcedb01472
hamidahclaudia <33897730+hamidahclaudia@users.noreply.github.com>
2019-01-21 19:35:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ module.exports = {
     ENV: process.env.NODE_ENV || 'development',
     PORT: process.env.PORT || '3000',
     URL: process.env.BASE_URI || 'http://localhost:3000',
-    MONGODB_URI: process.env.MONGODB_URI || 'mongodb://claudiahamidah:0135hore@ds155614.mlab.com:55614/rentcar',
+    MONGODB_URI: process.env.MONGODB_URI || 'mongodb://<claudiahamidah>:<0135hore>@ds261114.mlab.com:61114/testgigelid',
     JWT_SECRET: process.env.JWT_SECRET || 'secret1'
 }
